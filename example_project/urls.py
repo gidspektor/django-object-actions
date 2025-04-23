514dd6c84678bf2ff60ce3b8e19bd9959f31afa3
Gideon Spektor <39449284+gidspektor@users.noreply.github.com>
2025-04-23 10:41:12 -0600

    removed patterns

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,26 +1,13 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
+from django.views.static import serve
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'example_project.views.home', name='home'),
-    # url(r'^example_project/', include('example_project.foo.urls')),
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
-
-
-# serve media
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-   )
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
